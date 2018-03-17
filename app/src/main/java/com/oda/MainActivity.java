637a3cf51e4270a6597f43866412e7d583c1b9f0
aarondomo <lrc_aaron@hotmail.com>
2018-03-17 14:17:35 -0400

    Add menu

--- app/src/main/java/com/oda/MainActivity.java
+++ app/src/main/java/com/oda/MainActivity.java
@@ -1,7 +1,11 @@
-package com.oda.oda;
+package com.oda;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.oda.oda.R;
+import com.oda.ui.HomeActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startActivity(new Intent(this, HomeActivity.class));
     }
 }
